47f468a70c1fb41f994e6af437007d4bb3b24b50
Elias <just.elias4@gmail.com>
2022-02-28 22:27:00 +0100

    added time

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,20 +1,46 @@
 <!-- Please remove this file from your project -->
 <template>
-<div class="contianer mx-auto">
+<div class="h-screen w-screen flex place-items-center justify-center">
 
-  <div class="text-center">
-    01
+  <div class="text-9xl">
+    {{new Date(timeleft)}}
   </div>
-
 
 </div>
 </template>
 
 <script>
+
 export default {
-  name: 'NuxtTutorial'
+  name: 'NuxtTutorial',
+
+  methods:{
+    countTime(){
+      let time=timeleft.sprit(',')
+      time[1]++;
+
+    }
+
+
+
+  },
+  data(){
+    return {
+      timeleft:'03:40',
+      time:3
+    }
+  },
+  mounted() {
+  //  countTime();
+  },
+
+
 }
+
+
 </script>
+
 <style>
+
 
 </style>
